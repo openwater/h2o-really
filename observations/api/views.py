fc0e86c50dcf6748d25825024bae224f6c589e95
James Rutherford <jim@jimr.org>
2013-08-31 12:05:37 +0100

    Added a compact measurement serializer
    Reduces API response size by about 70%. Having done that, we'll need a
    new measurement details API for getting the full info for each one.

--- observations/api/views.py
+++ observations/api/views.py
@@ -1,9 +1,16 @@
 from rest_framework import generics
 
 from observations.models import Measurement
-from observations.api.serializers import MeasurementSerializer
+from observations.api.serializers import (
+    CompactMeasurementSerializer, MeasurementSerializer,
+)
 
 
 class MeasurementList(generics.ListAPIView):
-    serializer_class = MeasurementSerializer
     queryset = Measurement.objects.all()
+
+    def get_serializer_class(self):
+        if self.request.QUERY_PARAMS.get('compact'):
+            return CompactMeasurementSerializer
+        else:
+            return MeasurementSerializer
