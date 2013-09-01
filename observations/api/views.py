7a5578035b0b6d5543fd6ccecb828c76a9225285
James Rutherford <jim@jimr.org>
2013-09-01 21:26:35 +0100

    Minor refactoring of filter code + conncted to map
    Filter is now passed into the map page context & renders a form we use
    for updating the map. The 'observed' field basically checks the key in
    the hstore 'observations' field, so based on the bootstrap data, valid
    lookups are 'Nitrates', and 'Orthophosphates'. Dates should be
    YYYY-MM-DD, but really we need to hook up some kind of calendar widget
    here.

--- observations/api/views.py
+++ observations/api/views.py
@@ -1,29 +1,16 @@
-import django_filters
 from rest_framework import generics
 
+from observations.filters import MeasurementFilter
 from observations.models import Measurement
 from observations.api.serializers import (
     CompactMeasurementSerializer, MeasurementSerializer,
 )
 
 
-class MeasurementFilter(django_filters.FilterSet):
-    from_date = django_filters.DateFilter(
-        name='reference_timestamp', lookup_type='gte',
-    )
-    to_date = django_filters.DateFilter(
-        name='reference_timestamp', lookup_type='lte',
-    )
-
-    class Meta:
-        model = Measurement
-        fields = ['from_date', 'to_date']
-
-
 class MeasurementList(generics.ListAPIView):
     serializer_class = MeasurementSerializer
     filter_class = MeasurementFilter
-    queryset = Measurement.objects.all()
+    queryset = Measurement.observations_manager.all()
 
     def get_serializer_class(self):
         if self.request.is_ajax() or self.request.QUERY_PARAMS.get('compact'):
