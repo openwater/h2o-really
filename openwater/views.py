a5e5d5402e49d22db2681f70487218b71e960e66
Steve Pike <stringfellow@sucs.org>
2013-09-15 17:04:59 +0100

    updated home page

--- openwater/views.py
+++ openwater/views.py
@@ -3,7 +3,14 @@
 
 from django.views.generic.base import TemplateView
 
+from diario.models import Entry
+
 
 class HomePageView(TemplateView):
 
     template_name = "home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(HomePageView, self).get_context_data(**kwargs)
+        context['entry_list'] = Entry.objects.all()[:3]
+        return context
